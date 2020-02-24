91c56d29e89d819db56f704d95552b3e94313291
musta55 <48786719+musta55@users.noreply.github.com>
2020-02-24 12:43:47 +0600

    Emotion Calculation And polarity

--- EmotionCalculation.java
+++ EmotionCalculation.java
@@ -1,61 +1,155 @@
 
 package spl1;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmotionCalculation {
     public String[] emotion= new String[85000];
      public String[] word= new String[85000];
-     public ArrayList<String>outList=new ArrayList();
-    int i=0;
-     public  void emotionCalc() throws IOException {
+    int i=0,k=0,totalEmotionCount=0;
+    int Frequency[] = new int[6];
+    public ArrayList<String> emotionList= new ArrayList<String>();      // Emotion Word List
+    public ArrayList<String> wordList= new ArrayList<String>();         //Finding emotion from word
+
+
+
+    public  void searchEmotion() throws IOException {
 
         //get the file
-        File file = new File("C:\\Users\\User\\Desktop\\emotion.txt");
-        
-        Scanner sc = new Scanner(file);
-
-        while (sc.hasNextLine()){                                     
-            emotion[i] = sc.nextLine();                                  
-
-            String[] firstSplit= emotion[i].split("\t",0);
-            word[i]=firstSplit[0];
-            emotion[i]=firstSplit[1];
+        System.out.print("After Lemmatization : ");
+        for(i=0; i < Operations.outList.size(); i++) {
+            System.out.print(Operations.outList.get(i)+" ");
         }
-        i++;
+        System.out.println("");
+        File files = new File("src/spl1/emotion.txt");
 
-}
+        FileReader fr = new FileReader(files);
+        BufferedReader br =new BufferedReader(fr);
 
-    public void searchEmotion()
-        {
-           Operations op=new Operations(outList);
-
-            outList=op.getOutList();
-            Boolean found = false;
+        String currentLine;
 
 
-            for(i=0; i < op.getOutList().size(); i++){
+        int p=0;
+        while ((currentLine = br.readLine())!=null){
 
-                System.out.println("sadsa");
-                found = false;
-                NEW:
-                for (int j=0; j<word.length; j++){
+            String[] firstSplit= currentLine.split("\t",0);
+            wordList.add(firstSplit[0]);
+            emotionList.add(firstSplit[1]);
+            p++;
 
-                        if(op.getOutList().get(j).equals(word[j])){
-                            System.out.print("Emotion :"+emotion[j]+" ");
-                            found = true;
-                            break NEW;
-                        }
-                    }
-                }
-                if(!found){
-                    System.out.print(op.outList.get(i)+" ");
+        }
+
+//        for(int j=0;j<wordList.size();j++)
+//        {
+//            System.out.println("word="+wordList.get(j));
+//        }
+//
+//        for(int j=0;j<emotionList.size();j++)
+//        {
+//            System.out.println(" emotion="+emotionList.get(j));
+//        }
+
+        //   Operations op=new Operations(outList);
+
+        //   outList=op.getOutList();
+        Boolean found = false;
+        for(int w=0;w<6;w++)Frequency[w]=0;
+
+        for(i=0; i < Operations.outList.size(); i++) {
+
+
+            found = false;
+            NEW:
+            for (int j = 0; j < wordList.size(); j++) {
+
+                //   System.out.println(wordList.get(j)+"  ");
+
+                if (Operations.outList.get(i).equals(wordList.get(j))) {
+                    //   System.out.println("sadsa");
+                    System.out.print("Emotion :" + emotionList.get(j) + " ");
+                    if(emotionList.get(j).equalsIgnoreCase("joy"))Frequency[0]++;
+                    if(emotionList.get(j).equalsIgnoreCase("surprise"))Frequency[1]++;
+                    if(emotionList.get(j).equalsIgnoreCase("fear"))Frequency[2]++;
+                    if(emotionList.get(j).equalsIgnoreCase("sadness"))Frequency[3]++;
+                    if(emotionList.get(j).equalsIgnoreCase("anger"))Frequency[4]++;
+                    if(emotionList.get(j).equalsIgnoreCase("disgust"))Frequency[5]++;
+
+
+                    found = true;
+                    break NEW;
                 }
             }
+
+            if (!found) {
+                //     System.out.print(outList.get(i) + " ");
+                System.out.print("Emotion : Neutral ");
+            }
         }
+
+
+
+
+
+
+    }
+
+    public void emotionCalc()
+    {
+
+
+        int max=-1,temp=0;
+
+        for(int iteration=0;iteration<6;iteration++)
+        {
+            if(Frequency[iteration]>=max)
+            {
+                max=Frequency[iteration];
+                temp=iteration;
+            }
+
+        }
+
+        totalEmotionCount=4*Frequency[0]+3*Frequency[1]+(-1)*Frequency[2]+(-2)*Frequency[3]+(-3)*Frequency[4]+(-4)*Frequency[5];
+
+        for(int w=0;w<6;w++)
+        {
+            System.out.println(Frequency[w]);
+        }
+
+        System.out.println("Total emotion score :"+totalEmotionCount);
+        if(totalEmotionCount>0) System.out.println("Overall the sentence is positive ");
+        else if(totalEmotionCount==0) System.out.println("Overall the sentence is neutral");
+        else System.out.println("Overall the sentence is negative ");
+        if(max==0) System.out.println("Emotion Preference is neutral");
+
+        else {
+            System.out.println("");
+            if (temp == 0) {
+                System.out.println("Emotion Preference is Joy");
+            }
+            if (temp == 1) {
+                System.out.print("Emotion Preference is Surprised");
+            }
+            if (temp == 2) {
+                System.out.print("Emotion Preference is Fear");
+            }
+            if (temp == 3) {
+                System.out.print("Emotion Preference is Sadness");
+            }
+            if (temp == 4) {
+                System.out.print("Emotion Preference is Anger");
+            }
+            if (temp == 5) {
+                System.out.print("Emotion Preference is Disgust");
+            }
+
+
+        }
+
+    }
+
+}
 
 
